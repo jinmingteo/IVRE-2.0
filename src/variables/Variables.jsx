3d28c69208728104c72c37d39b933151db66beab
Tianran <matianran9600@hotmail.com>
2018-04-19 23:31:56 +0800

    leaderboard trial

--- src/variables/Variables.jsx
+++ src/variables/Variables.jsx
@@ -247,14 +247,14 @@ var style = {
 // // // For tables
 // //
 //
-const thArray = ["ID", "Name", "Salary", "Country", "City"];
+const thArray = ["Rank", "Name","Total Completion"];
 const tdArray = [
-  ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
-  ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
-  ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
-  ["4", "Philip Chaney", "$38,735", "Korea, South", "Overland Park"],
-  ["5", "Doris Greene", "$63,542", "Malawi", "Feldkirchen in Kärnten"],
-  ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"]
+  ["1", "Dakota Rice", "$36,738"],
+  ["2", "Minerva Hooper", "$23,789"],
+  ["3", "Sage Rodriguez", "$56,142"],
+  ["4", "Philip Chaney", "$38,735"],
+  ["5", "Doris Greene", "$63,542"],
+  ["6", "Mason Porter", "$78,615"]
 ];
 
 //
