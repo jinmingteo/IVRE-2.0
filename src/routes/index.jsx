17a8b1f70f743997a52c1daa706b99baf04ebe54
jinmingteo <jinmingteo95@gmail.com>
2018-04-20 13:56:11 +0800

    debugggggg

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import Dashboard from "layouts/Dashboard/Dashboard.jsx";
 
-var indexRoutes = [{ path: "/", name: "Home", component: Dashboard }];
+var indexRoutes = [{path: "/", name: "Home", component: Dashboard}];
 
 //, { path: "instrudashboard", name: "Instructor", component: }
 export default indexRoutes;
